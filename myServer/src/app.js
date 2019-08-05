1fbc41ba6b6e872c89baf562563b316a703f6862
xss8352 <xss8352@closeli.cn>
2019-08-05 18:07:45 +0800

    19/08/25 mongo操作

--- myServer/src/app.js
+++ myServer/src/app.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var path = require('path');
 var webpack = require('webpack');
+const bodyParser = require("body-parser");
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var app = express();
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-   app.use('./static', express.static(__dirname + '/dist'));
+mongoose.connect('mongodb://localhost/xss', { useNewUrlParser: true }, (err,res) => {
+  if(!err) {
+    console.log('连接成功')
+  } else {
+    console.log('连接失败')
+  }
+});
+
+// var MySchema = new Schema;
+var MySchema = new Schema({ name: String, color: String, sex: String, age: Number, createTime: String, _id: String}, {collation: 'data'});
+// MySchema.add({ name: 'string', color: 'string', price: 'number' })
+// MySchema.add({ name: String, color: String, sex: String, age: Number, createTime: String, _id: String})
+
+app.use('./static', express.static(__dirname + '/dist'));
 // app.get('/', function(req, res) {
 //   // res.send('Hello World');
 //   res.sendFile(path.resolve("./dist/index.html"));
@@ -35,15 +51,100 @@ app.use(webpackDevMiddleware(compiler, {
     colors: true
   }
 }));
+app.all('*', function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.setHeader("Content-Type", "application/json;charset=utf-8");
+
+  next()
+})
 app.get('/process_get', function(req, res) {
+
   var response = {
-      "first_name":req.query.first_name,
-      "last_name":req.query.last_name
+      "xing":req.query.first_name,
+      "ming":req.query.last_name
   };
-  console.log(response);
+  
   res.end(JSON.stringify(response));
 })
-var server =  app.listen(8082, function() {
+app.post('/process_post', bodyParser.json(), function(req, res) {
+  let params = req.body;
+  let {first_name, last_name} = params;
+  var response = {
+      "xingPost": first_name,
+      "mingPost": last_name
+  };
+  
+  res.end(JSON.stringify(response));
+})
+app.post('/cat/add', bodyParser.json(), function(req, res) {
+  let params = req.body;
+  let { name, color, sex, age } = params;
+  var response = {
+    "code": 200,
+    "message": 'success'
+  };
+  var resErr = {
+    "code": 10000,
+    "message": 'insert error'
+  };
+  const Cat = mongoose.model('Cat', MySchema);
+  const kitty = new Cat({ id: new mongoose.Schema.Types.ObjectId, name, color, sex, age, createTime: +new Data() });
+  kitty.save((err, doc) => {
+    if(!err) {
+      res.end(JSON.stringify(response));
+    } else {
+      res.end(JSON.stringify(resErr));
+    }
+  })/* .then(() => {
+    debugger
+  }) */;
+  
+  
+})
+app.post('/cat/list', bodyParser.json(), function(req, res) {
+  let params = req.body;
+  // let { name, color, sex, age } = params;
+  var response = {
+    "code": 200,
+    "message": 'success'
+  };
+  var resErr = {
+    "code": 10000,
+    "message": 'insert error'
+  };
+  // res.end(JSON.stringify(response));
+  const Cat = mongoose.model('Cat', MySchema);
+  Cat.find({}, 'name color sex age createTime').sort({name: 1}).collation({locale: 'en_US', numericOrdering: true})
+    .then(docs => {
+  // .populate([{path: 'name'}, {path: 'color'}, {path: 'sex'}, {path: 'age'},{path: 'createTime'} ])
+  // .exec(function (err,result) {
+    response.list = docs||[];
+    debugger
+    res.end(JSON.stringify(response));
+    // if(!err) {
+    //   response.list = result;
+    //   res.end(JSON.stringify(response));
+    // } else {
+    //   res.end(JSON.stringify(resErr));
+    // }
+  })
+  // const kitty = new Cat({ name, color, sex, age, createTime: moment().format('YYYY-MM-DD HH:mm:ss') });
+  /* kitty.save((err, doc) => {
+    if(!err) {
+      res.end(JSON.stringify(response));
+    } else {
+      res.end(JSON.stringify(resErr));
+    }
+  }).then(() => {
+    debugger
+  }) */;
+  
+  
+})
+var server =  app.listen(8088, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
--- webpack4_react/src/components/Comment.jsx
+++ webpack4_react/src/components/Comment.jsx
@@ -1,33 +1,265 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xushanshan
+ * @Date: 2019-08-02 15:25:24
+ * @LastEditors: xushanshan
+ * @LastEditTime: 2019-08-05 17:32:07
+ */
 import React from 'react'
-import { Form, Input } from 'antd';
+import axios from 'axios'
+import moment from 'moment'
+import { Table, Divider, Tag, Result, Form, Input, Button } from 'antd';
 import './style.scss'
 
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+// const OPTIONS = {
+//   headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+// }
+const OPTIONS = {
+  headers: {'Content-Type': 'application/json;charset=utf-8'}
+}
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a href="javascript:;">{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Color',
+    dataIndex: 'color',
+    key: 'color',
+  },
+  {
+    title: 'Sex',
+    dataIndex: 'sex',
+    key: 'sex',
+  },
+  {
+    title: 'CreateTime',
+    dataIndex: 'createTime',
+    key: 'createTime',
+    render: createTime => (
+      <span>
+        {moment(createTime).format('YYYY-MM-DD HH:mm:ss')}
+      </span>
+    )
+  },
+  // {
+  //   title: 'Tags',
+  //   key: 'tags',
+  //   dataIndex: 'tags',
+  //   render: tags => (
+  //     <span>
+  //       {tags.map(tag => {
+  //         let color = tag.length > 5 ? 'geekblue' : 'green';
+  //         if (tag === 'loser') {
+  //           color = 'volcano';
+  //         }
+  //         return (
+  //           <Tag color={color} key={tag}>
+  //             {tag.toUpperCase()}
+  //           </Tag>
+  //         );
+  //       })}
+  //     </span>
+  //   ),
+  // },
+  // {
+  //   title: 'Action',
+  //   key: 'action',
+  //   render: (text, record) => (
+  //     <span>
+  //       <a href="javascript:;">Invite {record.name}</a>
+  //       <Divider type="vertical" />
+  //       <a href="javascript:;">Delete</a>
+  //     </span>
+  //   ),
+  // },
+];
+
+/* const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+]; */
 
 class Comment extends React.Component{
-  handleSubmit(e) {
+  state = {
+      repoName:'',
+      repoUrl:'',
+      getRst: false,
+      rstStatus: null,
+      catsList: []
+  };
+  componentDidMount(){
+    this.initTable()
+  }
+  initTable() {
+    const url = `http://127.0.0.1:8088/cat/list`;
+    let data = {};
+    let promise = axios.post(url, data, OPTIONS);
+
+    promise.then(response=>{
+      console.log(response);
+      const result = response.data;
+        // const {xing,ming} = result;
+        
+      this.setState({
+        catsList: result.list
+      })
+    });
+  }
+
+  handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        // console.log('Received values of form: ', values);
+        // const url = `https://api.github.com/search/repositories?q=r&sort=stars`;
+        const url = `http://127.0.0.1:8088/process_get?first_name=xu&last_name=shanshan`;
+        // const url = `my_test`;
+        let promise = axios.get(url);
+        promise.then(response=>{
+            console.log(response);
+            const result = response.data;
+            const {xing,ming} = result;
+            
+            this.setState({
+                repoName:xing,
+                repoUrl:ming
+            })
+        });
+      }
+    });
+  };
+  handleSubmitPost = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        // const url = `https://api.github.com/search/repositories?q=r&sort=stars`;
+        // const url = `http://127.0.0.1:8088/process_post`;//?first_name=xu&last_name=shanshan
+        const url = `http://127.0.0.1:8088/cat/add`;
+        // const url = `my_test`;
+        let { name, color, sex, age} = values;
+        let data = {name, color, sex, age};
+        let promise = axios.post(url, data, OPTIONS);
+        promise.then(response=>{
+            console.log(response);
+            const data = response.data;
+            // const {xingPost,mingPost} = result;
+            if(data.code == 200) {
+              this.setState({getRst: true, rstStatus: 1})
+            } else {
+              this.setState({getRst: true, rstStatus: 0})
+            }
+        });
       }
     });
   };
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const { getFieldDecorator, getFieldsError } = this.props.form;
     
     return (
       <div className="myForm">
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
+        <Form className="login-form">
+          <Form.Item label="name">
+          {getFieldDecorator('name', {
+            rules: [{ required: true, message: 'Please input your name!' }],
           })(
             <Input
-              placeholder="Username"
+              placeholder="name"
             />,
           )}
           </Form.Item>
+          <Form.Item label="color">
+          {getFieldDecorator('color', {
+            rules: [{ required: true, message: 'Please input your color!' }],
+          })(
+            <Input
+              placeholder="color"
+            />,
+          )}
+          </Form.Item>
+          <Form.Item label="sex" help="Should be 'F' or 'M'">
+          {getFieldDecorator('sex', {
+            rules: [{ required: true, message: 'Please input your sex!' }],
+          })(
+            <Input
+              placeholder="sex"
+            />,
+          )}
+          </Form.Item>
+          <Form.Item label="age" help="Should be numbers">
+          {getFieldDecorator('age', {
+            rules: [{ required: true, message: 'Please input your age!' }],
+          })(
+            <Input
+              placeholder="age"
+            />,
+          )}
+          </Form.Item>
+          <Form.Item>
+            {/* <Button type="primary" htmlType="button" onClick={this.handleSubmit} disabled={hasErrors(getFieldsError())}>
+              Log in GET
+            </Button> */}
+            <Button type="primary" htmlType="button" onClick={this.handleSubmitPost} disabled={hasErrors(getFieldsError())}>
+              Add我的🐱
+            </Button>
+          </Form.Item>
         </Form>
+        <Button type="primary" htmlType="button" onClick={this.initTable.bind(this)}>
+          获取list
+        </Button>
+        <Table columns={columns} dataSource={this.state.catsList} />
+        {this.state.getRst && (this.state.rstStatus == 1?<Result
+              status="success"
+              title="Successfully!"
+              subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+              extra={[
+                <Button type="primary" key="console">
+                  Go Console
+                </Button>,
+                <Button key="buy">Buy Again</Button>,
+              ]}
+            />:<Result
+            status="error"
+            title="Submission Failed"
+            subTitle="Please check and modify the following information before resubmitting."
+            extra={[
+              <Button type="primary" key="console">
+                Go Console
+              </Button>,
+              <Button key="buy">Buy Again</Button>,
+            ]}
+        />)}
       </div>
     )
   }
